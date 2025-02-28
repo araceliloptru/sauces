c2f321e96c027c3c0f981ed7b3836ea9247a68f9
ARACELI LOPEZ TRUJILLO <araceli.loptru@educa.jcyl.es>
2025-02-28 10:08:07 +0100

    Prueba commit

---
+++ Ej1Hoja8.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Ej1Hoja8{
+    public static void main(String[] args) {
+        Scanner teclado=new Scanner(System.in);
+						
+			for(int i=1;i<=5;i++) {
+				escribirTabla(i);
+			}
+	}
+		private static void escribirTabla(int i){
+			for (int j=1;j<=10;j++) {
+					System.out.println(i+"*"+j+"="+(i*j));
+				}
+		}
+}
---
+++ Ej2-A01.java
@@ -0,0 +1,14 @@
+
+public class Ej2-A01{
+	public static void main (String[] args) {
+		
+	int numero1=5;
+	int numero2=2;
+	
+	System.out.println(numero1+numero2);
+	System.out.println(numero1-numero2);
+	System.out.println(numero1*numero2);
+	System.out.println(numero1/numero2);
+	
+	}
+}
